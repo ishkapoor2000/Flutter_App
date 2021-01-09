9042cef0ab1b759178f150e590949437d146c922
Ish Kapoor <60035881+ishkapoor2000@users.noreply.github.com>
2021-01-09 15:39:26 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,10 @@ class RandomWordsState extends State<RandomWords> {
   final TextStyle _biggerFont = const TextStyle(fontSize: 18); 
   @override
   Widget build(BuildContext context) {
-//    final wordPair = WordPair.random(); // Delete these... 
-//    return Text(wordPair.asPascalCase); // ... two lines.
+//    final wordPair = WordPair.random(); 
+//    return Text(wordPair.asPascalCase);
 
-    return Scaffold (                   // Add from here... 
+    return Scaffold ( 
       appBar: AppBar(
         title: Text('Startup Name Generator: Ish'),
       ),
@@ -31,30 +31,20 @@ class RandomWordsState extends State<RandomWords> {
   Widget _buildSuggestions() {
     return ListView.builder(
       padding: const EdgeInsets.all(16),
-      // The itemBuilder callback is called once per suggested 
-      // word pairing, and places each suggestion into a ListTile
-      // row. For even rows, the function adds a ListTile row for
-      // the word pairing. For odd rows, the function adds a 
-      // Divider widget to visually separate the entries. Note that
-      // the divider may be difficult to see on smaller devices.
+      // The itemBuilder callback is called once per suggested word pairing, and places each suggestion into a ListTile row. For even rows, the function adds a ListTile row for the word pairing. For odd rows, the function adds a Divider widget to visually separate the entries. Note that the divider may be difficult to see on smaller devices.
       itemBuilder: (BuildContext _context, int i) {
-        // Add a one-pixel-high divider widget before each row 
-        // in the ListView.
+        // Add a one-pixel-high divider widget before each row in the ListView.
         if (i.isOdd) {
           return Divider();
         }
 
-        // The syntax "i ~/ 2" divides i by 2 and returns an 
-        // integer result.
+        // The syntax "i ~/ 2" divides i by 2 and returns an integer result.
         // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
-        // This calculates the actual number of word pairings 
-        // in the ListView,minus the divider widgets.
+        // This calculates the actual number of word pairings in the ListView,minus the divider widgets.
         final int index = i ~/ 2;
-        // If you've reached the end of the available word
-        // pairings...
+        // If you've reached the end of the available word pairings...
         if (index >= _suggestions.length) {
-          // ...then generate 10 more and add them to the 
-          // suggestions list.
+          // ...then generate 10 more and add them to the suggestions list.
           _suggestions.addAll(generateWordPairs().take(10));
         }
         return _buildRow(_suggestions[index]);
